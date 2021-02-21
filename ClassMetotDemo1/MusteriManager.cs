f86e926fdbc8d80d1aaae4e77d8b3014ec05e1a5
elifn <elifn@LAPTOP-VAHU78U7>
2021-02-21 22:06:44 +0300

    Add project files.

---
+++ ClassMetotDemo1/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo1
+{
+    class MusteriManager
+    {
+        public void Add(Musteri Musteri)
+        {
+            Console.WriteLine("Adı:" + Musteri.Adi + " " + "Soyadı:" + Musteri.Soyadi + " " + "Id:" + Musteri.Id + " ");
+            Console.WriteLine("Müşteri eklendi.");
+        }
+
+        public void Delete(Musteri Musteri)
+        {
+            Console.WriteLine("Adı:" + Musteri.Adi + " " + "Soyadı:" + Musteri.Soyadi + " " + "Id:" + Musteri.Id + " ");
+            Console.WriteLine("Müşteri silindi.");
+
+        }
+
+        public void List(Musteri Musteri)
+        {
+            Console.WriteLine("Adı:" + Musteri.Adi + " " + "Soyadı:" + Musteri.Soyadi + " " + "Id:" + Musteri.Id + " ");
+            Console.WriteLine("Müşteri listesi");
+
+        }
+    }
+}
---
+++ ClassMetotDemo1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ClassMetotDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 0338297;
+            musteri1.Adi = "Elif";
+            musteri1.Soyadi = "Mandal";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 7236846;
+            musteri2.Adi = "Zeynep";
+            musteri2.Soyadi = "Tatlı";
+
+            MusteriManager musterimanager = new MusteriManager();
+            Musteri[] Musteriler = new Musteri[] { musteri1, musteri2 };
+
+            foreach (Musteri Musteri in Musteriler)
+            {
+                musterimanager.List(Musteri);
+            }
+
+            musterimanager.Add(musteri2);
+            musterimanager.Delete(musteri1);
+
+        }
+    }
+    
+}
